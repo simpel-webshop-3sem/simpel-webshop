a81b98a90a4e7985d8a386969deaa07082fa1ec1
buchhavee <mach0015@stud.kea.dk>
2025-11-17 11:56:14 +0100

    Refactor CartList component layout and update Menu styles for improved UI

--- src/app/(routes)/productlist/components/CartList.jsx
+++ src/app/(routes)/productlist/components/CartList.jsx
@@ -1,24 +1,26 @@
 const CartList = () => {
   return (
-    <div className="w-lg p-4 pt-24">
-      <div className="bg-gray-200 p-4">
-        <h2 className="mb-4 text-sm font-bold tracking-wider text-black uppercase">
-          Shopping Cart
-        </h2>
+    <div className="fixed top-40 right-0 w-md p-4">
+      <div className="min-h-[500px] border-l border-black/20 pl-4">
+        <div className="flex min-h-[500px] flex-col bg-gray-200 p-4">
+          <h2 className="mb-4 text-sm font-bold tracking-wider text-black uppercase">
+            Shopping Cart
+          </h2>
 
-        <div className="flex flex-col gap-4">
-          <div className="border-t border-gray-400 pt-4">
-            <div className="flex justify-between">
-              <span className="text-sm font-bold tracking-wider text-black uppercase">
-                Total
-              </span>
-              <span className="text-sm font-bold text-black">$ 0</span>
+          <div className="flex flex-1 flex-col justify-end gap-4">
+            <div className="border-t border-gray-400 pt-4">
+              <div className="flex justify-between">
+                <span className="text-sm font-bold tracking-wider text-black uppercase">
+                  Total
+                </span>
+                <span className="text-sm font-bold text-black">$ 0</span>
+              </div>
             </div>
-          </div>
 
-          <button className="w-full bg-gray-300 py-3 text-xs font-bold tracking-wider text-black uppercase hover:bg-gray-400">
-            Checkout
-          </button>
+            <button className="w-full bg-gray-300 py-3 text-xs font-bold text-black uppercase hover:bg-gray-400">
+              Checkout
+            </button>
+          </div>
         </div>
       </div>
     </div>
